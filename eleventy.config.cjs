eb8903d1591485460c7d3137bd1a61bf496237fe
Chloé <47738241+rooselle@users.noreply.github.com>
2024-07-25 16:13:36 +0200

    chore: add layout

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,4 +1,11 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/img')
+  eleventyConfig.addPassthroughCopy('src/browserconfig.xml')
+  eleventyConfig.addPassthroughCopy('src/favicon.ico')
+  eleventyConfig.addPassthroughCopy('src/robots.txt')
+  eleventyConfig.addPassthroughCopy('src/site.webmanifest')
+  eleventyConfig.addPassthroughCopy('src/sitemap.xml')
+
   return {
     dir: {
       input: 'src',
